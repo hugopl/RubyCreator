75822b0897d280be0cc3c60f284e9988a27c7617
Hugo Parente Lima <hugo.pl@gmail.com>
2014-04-08 13:30:01 -0300

    Move RubyEditor to it's own directory, so the RubyProject plugin can stay on this very same repository.

--- meique.lua
+++ meique.lua
@@ -9,43 +9,9 @@ s = f:read("*all")
 f:close()
 _, _, QTCREATOR_VERSION = string.find(s, "QTCREATOR_VERSION = (3.0.%d)")
 
-configureFile("RubyEditor.pluginspec.in", "RubyEditor.pluginspec")
-
 QT_VERSION = "4.8"
 qtCore = findPackage("QtCore", QT_VERSION)
 qtGui = findPackage("QtGui", QT_VERSION)
 ruby = findPackage("ruby-2.1")
 
-plugin = Library:new("RubyEditor")
-plugin:useQtAutomoc()
-plugin:use(qtCore)
-plugin:use(qtGui)
-plugin:use(ruby)
-
--- QtCreator include paths
-plugin:addIncludePath(qtcSrc.."/src/plugins")
-plugin:addIncludePath(qtcSrc.."/src/libs")
-
-plugin:addFiles([[
-    RubyEditor.cpp
-    RubyEditorPlugin.cpp
-    RubyEditorFactory.cpp
-    RubyEditorWidget.cpp
-    RubyHighlighter.cpp
-    RubyScanner.cpp
-    RubyCurrentDocumentFilter.cpp
-    RubyCodeModel.cpp
-]])
-plugin:addQtResource("RubyEditor.qrc")
-
--- Hardcoded QtCreator libraries path
-plugin:addLibraryPaths([[
-/usr/lib/qtcreator
-/usr/lib/qtcreator/plugins
-/usr/lib/qtcreator/plugins/QtProject
-]])
-
-plugin:addLinkLibraries([[
-Core
-TextEditor
-]])
+addSubdirectory("rubyeditor")

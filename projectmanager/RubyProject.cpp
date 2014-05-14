2de2d38dc9b2fc058981201f46a808fbfb283b13
Hugo Parente Lima <hugo.pl@gmail.com>
2014-05-14 12:36:04 -0300

    Monitor all folder to auto add/remove files from project.

--- projectmanager/RubyProject.cpp
+++ projectmanager/RubyProject.cpp
@@ -15,13 +15,15 @@ namespace Ruby {
 Project::Project(ProjectManager* projectManager, const QString& fileName)
     : m_projectManager(projectManager)
     , m_document(new Document)
-
+    , m_nameFilter({"*.rb", "*.erb", "config.ru", "Gemfile", "Rakefile"})
 {
     m_projectDir = QFileInfo(fileName).dir();
     m_rootNode = new ProjectNode(m_projectDir.dirName());
 
-    populateProject(m_projectDir, m_rootNode);
-    CodeModel::instance()->updateModels(m_files);
+    populateProject();
+    CodeModel::instance()->updateModels(files(AllFiles));
+
+    connect(&m_fsWatcher, &QFileSystemWatcher::directoryChanged, this, &Project::populateProject);
 }
 
 
@@ -47,31 +49,89 @@ ProjectExplorer::ProjectNode* Project::rootProjectNode() const
 
 QStringList Project::files(FilesMode) const
 {
-    return m_files;
+    return QStringList(m_files.toList());
 }
 
-void Project::populateProject(const QDir& dir, ProjectExplorer::FolderNode* parent)
+void Project::populateProject()
+{
+    QSet<QString> oldFiles(m_files);
+    m_files.clear();
+    recursiveScanDirectory(m_projectDir, m_files);
+    emit fileListChanged();
+
+    removeNodes(oldFiles - m_files);
+    addNodes(m_files - oldFiles);
+}
+
+void Project::recursiveScanDirectory(const QDir& dir, QSet<QString>& container)
+{
+    for (const QFileInfo& info : dir.entryInfoList(m_nameFilter, QDir::AllDirs | QDir::Files | QDir::NoDotAndDotDot | QDir::NoSymLinks | QDir::CaseSensitive)) {
+        if (info.isDir())
+            recursiveScanDirectory(QDir(info.filePath()), container);
+        else
+            container << info.filePath();
+    }
+    m_fsWatcher.addPath(dir.absolutePath());
+}
+
+void Project::addNodes(const QSet<QString>& nodes)
 {
     using namespace ProjectExplorer;
 
-    static QStringList nameFilter = QStringList() << "*.rb" << "*.erb" << "config.ru" << "Gemfile" << "Rakefile";
-    QList<FolderNode*> folders;
-    QList<FileNode*> files;
+    const QLatin1Char sep('/');
+    QStringList path;
+    for (const QString& node : nodes) {
+        path = m_projectDir.relativeFilePath(node).split(sep);
+        path.pop_back();
+        FolderNode* folder = findFolderFor(path);
+        folder->addFileNodes(QList<FileNode*>() << new FileNode(node, SourceType, false));
+    }
+}
 
-    for (const QFileInfo& info : dir.entryInfoList(nameFilter, QDir::AllDirs | QDir::Files | QDir::NoDotAndDotDot | QDir::NoSymLinks | QDir::CaseSensitive)) {
-        if (info.isDir()) {
-            folders << new FolderNode(info.fileName());
-            parent->addFolderNodes(folders);
-            populateProject(QDir(info.filePath()), folders.first());
-            folders.clear();
-        } else {
-            files << new FileNode(info.filePath(), SourceType, false);
-            m_files << info.filePath();
+void Project::removeNodes(const QSet<QString>& nodes)
+{
+    using namespace ProjectExplorer;
+
+    const QLatin1Char sep('/');
+    QStringList path;
+
+    for (const QString& node : nodes) {
+        path = m_projectDir.relativeFilePath(node).split(sep);
+        path.pop_back();
+        FolderNode* folder = findFolderFor(path);
+
+        for (FileNode* file : folder->fileNodes()) {
+            if (file->path() == node) {
+                folder->removeFileNodes(QList<FileNode*>() << file);
+                break;
+            }
         }
     }
-    parent->addFileNodes(files);
+}
 
-    emit fileListChanged();
+ProjectExplorer::FolderNode* Project::findFolderFor(const QStringList& path)
+{
+    using namespace ProjectExplorer;
+    FolderNode* folder = m_rootNode;
+
+    for (const QString& part : path) {
+        bool folderFound = false;
+        for (FolderNode* subFolder : folder->subFolderNodes()) {
+            if (subFolder->displayName() == part) {
+                folder = subFolder;
+                folderFound = true;
+                break;
+            }
+        }
+        if (!folderFound) {
+            FolderNode* newFolder = new FolderNode(part);
+            folder->addFolderNodes(QList<FolderNode*>() << newFolder);
+            folder = newFolder;
+        }
+    }
+    return folder;
 }
 
 }
+
+#include "RubyProject.moc"
--- projectmanager/RubyProject.h
+++ projectmanager/RubyProject.h
@@ -4,6 +4,8 @@
 #include <projectexplorer/project.h>
 #include <projectexplorer/projectnodes.h>
 
+#include <QFileSystemWatcher>
+
 namespace Ruby {
 
 class Document;
@@ -12,6 +14,7 @@ class ProjectNode;
 
 class Project : public ProjectExplorer::Project
 {
+    Q_OBJECT
 public:
     Project(ProjectManager* projectManager, const QString& fileName);
     QString displayName() const override;
@@ -20,15 +23,26 @@ public:
     ProjectExplorer::ProjectNode* rootProjectNode() const override;
 
     QStringList files(FilesMode) const override;
+
+private slots:
+    void populateProject();
 private:
     ProjectManager* m_projectManager;
     Document* m_document;
     ProjectNode* m_rootNode;
 
     QDir m_projectDir;
-    QStringList m_files;
+    QSet<QString> m_files;
+    QFileSystemWatcher m_fsWatcher;
 
-    void populateProject(const QDir& dir, ProjectExplorer::FolderNode* parent);
+    const QStringList m_nameFilter;
+
+
+    void recursiveScanDirectory(const QDir& dir, QSet<QString>& container);
+    void addNodes(const QSet<QString>& nodes);
+    void removeNodes(const QSet<QString>& nodes);
+
+    ProjectExplorer::FolderNode* findFolderFor(const QStringList& path);
 };
 
 }

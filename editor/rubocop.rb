2b270780765c98fc677803b5800d4cbac0f0e272
Hugo Parente Lima <hugo.pl@gmail.com>
2016-02-04 00:21:56 -0300

    Fix rubocop highlight support for rubocop 0.36.0 and Ruby 2.3.
    Our implementation depends on a private rubocop API, so this kind of
    thing may happen from time to time.

--- editor/rubocop.rb
+++ editor/rubocop.rb
@@ -1,15 +1,25 @@
 # wraper for rubocop
 begin
   require 'rubocop'
+
+  $rubocop36 = Gem::Version.new(RuboCop::Version.version) >= Gem::Version.new('0.36.0')
 rescue LoadError
   abort 'Rubocop not installed or not found, if you use rvm, check if rubocop is installed on your system ruby.'
 end
 
 module RuboCop
   class Runner
-    def _inspect_code(code, path)
-      @formatter_set ||= Formatter::FormatterSet.new
-      inspect_file(RuboCop::ProcessedSource.new(code, path))[0]
+    if $rubocop36
+      def _inspect_code(code, path)
+        @formatter_set ||= Formatter::FormatterSet.new
+        # This would be configurable in the project settings someday
+        inspect_file(RuboCop::ProcessedSource.new(code, RUBY_VERSION.to_f, path))[0]
+      end
+    else
+      def _inspect_code(code, path)
+        @formatter_set ||= Formatter::FormatterSet.new
+        inspect_file(RuboCop::ProcessedSource.new(code, path))[0]
+      end
     end
   end
 end

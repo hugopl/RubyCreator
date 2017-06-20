caeefde2e25f1b9ee92c2f965c13e885d3ce71d5
Orgad Shaneh <orgads@gmail.com>
2017-06-20 22:59:30 +0300

    Remove unused constant

--- RubyConstants.h
+++ RubyConstants.h
@@ -1,14 +1,11 @@
 #ifndef Ruby_Constants_h
 #define Ruby_Constants_h
 
-#include <QtGlobal>
-
 namespace Ruby {
 namespace Constants {
 
 const char EditorId[] = "Ruby.RubyEditor";
 const char ProjectId[] = "Ruby.Project";
-const char ProjectFileId[] = "Ruby.ProjectFile";
 const char RubyIcon[] = ":/rubysupport/Ruby.png";
 const char SettingsId[] = "Ruby.Settings";
 const char SnippetGroupId[] = "Ruby.Snippets";

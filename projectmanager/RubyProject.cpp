486a5ad6d08f3f15f3ac59a6a032a917abb078fe
Orgad Shaneh <orgads@gmail.com>
2018-01-07 14:44:12 +0200

    Fix crash when closing a project while scanning

--- projectmanager/RubyProject.cpp
+++ projectmanager/RubyProject.cpp
@@ -7,6 +7,7 @@
 #include <QDebug>
 #include <QtConcurrent>
 #include <QFileInfo>
+#include <QFutureWatcher>
 #include <QRegularExpression>
 #include <QThread>
 
@@ -19,25 +20,26 @@ const int MIN_TIME_BETWEEN_PROJECT_SCANS = 4500;
 
 Project::Project(const Utils::FileName &fileName) :
     ProjectExplorer::Project(Constants::MimeType, fileName, [this] { scheduleProjectScan(); })
-    , m_populatingProject(false)
 {
-    m_projectDir = fileName.toFileInfo().dir();
-    m_rootNode = new ProjectNode(Utils::FileName::fromString(m_projectDir.dirName()));
+    setId(Constants::ProjectId);
+    m_projectDir = QDir(fileName.parentDir().toString());
 
     m_projectScanTimer.setSingleShot(true);
-    connect(&m_projectScanTimer, &QTimer::timeout, this, &Project::populateProject);
+    connect(&m_projectScanTimer, &QTimer::timeout, this, &Project::scanProjectNow);
 
-    QFuture<void> future = QtConcurrent::run(this, &Project::populateProject);
-    Core::ProgressManager::instance()->addTask(future, tr("Parsing Ruby Files"), Constants::RubyProjectTask);
+    scanProjectNow();
 
     connect(&m_fsWatcher, &QFileSystemWatcher::directoryChanged, this, &Project::scheduleProjectScan);
 
     setDisplayName(m_projectDir.dirName());
 }
 
-ProjectExplorer::ProjectNode *Project::rootProjectNode() const
+Project::~Project()
 {
-    return m_rootNode;
+    if (m_projectScanFuture.isRunning()) {
+        m_projectScanFuture.cancel();
+        m_projectScanFuture.waitForFinished();
+    }
 }
 
 void Project::scheduleProjectScan()
@@ -49,122 +51,70 @@ void Project::scheduleProjectScan()
             m_projectScanTimer.start();
         }
     } else {
-        populateProject();
+        scanProjectNow();
     }
 }
 
 void Project::populateProject()
 {
-    if (m_populatingProject)
-        return;
-    m_populatingProject = true;
-
     m_lastProjectScan.start();
     QSet<QString> oldFiles(m_files);
     m_files.clear();
-    recursiveScanDirectory(m_projectDir, m_files);
+    recursiveScanDirectory(m_projectDir);
+    if (m_projectScanFuture.isCanceled())
+        return;
 
     const auto removedFiles = oldFiles - m_files;
     const auto addedFiles = m_files - oldFiles;
-
-    removeNodes(removedFiles);
-    addNodes(addedFiles);
 
     for (const QString &file : removedFiles)
         CodeModel::instance()->removeSymbolsFrom(file);
     for (const QString &file : addedFiles)
         CodeModel::instance()->addFile(file);
-    m_populatingProject = false;
-
-    if (!addedFiles.isEmpty() || !removedFiles.isEmpty())
-        emit fileListChanged();
 }
 
-void Project::recursiveScanDirectory(const QDir &dir, QSet<QString> &container)
+void Project::scanProjectNow()
 {
+    if (isParsing()) {
+        m_projectScanTimer.setInterval(MIN_TIME_BETWEEN_PROJECT_SCANS);
+        m_projectScanTimer.start();
+        return;
+    }
+    emitParsingStarted();
+    m_projectScanFuture = QtConcurrent::run(this, &Project::populateProject);
+    auto *watcher = new QFutureWatcher<void>();
+    watcher->setFuture(m_projectScanFuture);
+    Core::ProgressManager::instance()->addTask(m_projectScanFuture, tr("Parsing Ruby Files"), Constants::RubyProjectTask);
+    connect(watcher, &QFutureWatcher<void>::finished,
+            this, [this, watcher] {
+        auto newRoot = new ProjectNode(projectDirectory());
+        for (const QString &f : m_files) {
+            const Utils::FileName path = Utils::FileName::fromString(f);
+            newRoot->addNestedNode(new ProjectExplorer::FileNode(
+                                       path, ProjectExplorer::FileNode::fileTypeForFileName(path), false));
+        }
+        setRootProjectNode(newRoot);
+        emitParsingFinished(true);
+        watcher->deleteLater();
+    });
+}
+
+void Project::recursiveScanDirectory(const QDir &dir)
+{
+    if (m_projectScanFuture.isCanceled())
+        return;
     QRegularExpression projectFilePattern(".*\\.rubyproject(?:\\.user)?$");
     const auto files = dir.entryInfoList(QDir::AllDirs | QDir::Files | QDir::NoDotAndDotDot
                                          | QDir::NoSymLinks | QDir::CaseSensitive);
     for (const QFileInfo &info : files) {
+        if (m_projectScanFuture.isCanceled())
+            return;
         if (info.isDir())
-            recursiveScanDirectory(QDir(info.filePath()), container);
+            recursiveScanDirectory(QDir(info.filePath()));
         else if (!projectFilePattern.match(info.fileName()).hasMatch())
-            container << info.filePath();
+            m_files << info.filePath();
     }
     m_fsWatcher.addPath(dir.absolutePath());
-}
-
-void Project::addNodes(const QSet<QString> &nodes)
-{
-    using namespace ProjectExplorer;
-
-    const QChar sep = '/';
-    QStringList path;
-    for (const QString &node : nodes) {
-        path = m_projectDir.relativeFilePath(node).split(sep);
-        path.pop_back();
-        FolderNode *folder = findFolderFor(path);
-        folder->addNode(new FileNode(Utils::FileName::fromString(node), FileType::Source, false));
-    }
-}
-
-void Project::tryRemoveEmptyFolder(ProjectExplorer::FolderNode *folder)
-{
-    using namespace ProjectExplorer;
-
-    if (folder == m_rootNode)
-        return;
-
-    FolderNode *parent = folder->parentFolderNode();
-    if (parent && folder->fileNodes().empty() && folder->folderNodes().empty()) {
-        parent->removeNode(folder);
-        tryRemoveEmptyFolder(parent);
-    }
-}
-
-void Project::removeNodes(const QSet<QString> &nodes)
-{
-    using namespace ProjectExplorer;
-
-
-    for (const QString &node : nodes) {
-        QStringList path = m_projectDir.relativeFilePath(node).split('/');
-        path.pop_back();
-        FolderNode *folder = findFolderFor(path);
-
-        const auto files = folder->fileNodes();
-        for (FileNode *file : files) {
-            if (file->filePath().toString() == node) {
-                folder->removeNode(file);
-                tryRemoveEmptyFolder(folder);
-                break;
-            }
-        }
-    }
-}
-
-ProjectExplorer::FolderNode *Project::findFolderFor(const QStringList &path)
-{
-    using namespace ProjectExplorer;
-    FolderNode *folder = m_rootNode;
-
-    for (const QString &part : path) {
-        bool folderFound = false;
-        const auto subFolders = folder->folderNodes();
-        for (FolderNode *subFolder : subFolders) {
-            if (subFolder->displayName() == part) {
-                folder = subFolder;
-                folderFound = true;
-                break;
-            }
-        }
-        if (!folderFound) {
-            FolderNode *newFolder = new FolderNode(Utils::FileName::fromString(part));
-            folder->addNode(newFolder);
-            folder = newFolder;
-        }
-    }
-    return folder;
 }
 
 }

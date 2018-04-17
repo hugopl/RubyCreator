929a8dd6d27986b9e975b2fb3ac17077040634c7
Orgad Shaneh <orgads@gmail.com>
2018-04-17 23:06:52 +0300

    Adapt to master

--- projectmanager/RubyRunConfiguration.cpp
+++ projectmanager/RubyRunConfiguration.cpp
@@ -19,58 +19,40 @@ using namespace Utils;
 namespace Ruby {
 
 const char C_RUNCONFIGURATIONPREFIX[] = "Ruby.RunConfiguration.";
-const QString C_WORKINGDIRECTORYASPECT_ID = QStringLiteral("Ruby.RunConfiguration.WorkingDirectoryAspect");
-const QString C_ARGUMENTASPECT_ID = QStringLiteral("Ruby.RunConfiguration.ArgumentAspect");
-const QString C_TERMINALASPECT_ID = QStringLiteral("Ruby.RunConfiguration.TerminalAspect");
-const QString C_EXECUTABLE_KEY = QStringLiteral("Ruby.RunConfiguration.Executable");
-const QString C_WORKINGDIRECTORY_KEY = QStringLiteral("Ruby.RunConfiguration.WorkingDirectory");
-const QString C_COMMANDLINEARGS_KEY = QStringLiteral("Ruby.RunConfiguration.CommandlineArgs");
 
 RunConfiguration::RunConfiguration(Target *target)
-	: ProjectExplorer::RunConfiguration(target, C_RUNCONFIGURATIONPREFIX)
-	, m_workingDirectoryAspect(new WorkingDirectoryAspect(this, C_WORKINGDIRECTORYASPECT_ID))
-	, m_argumentAspect(new ArgumentsAspect(this, C_ARGUMENTASPECT_ID))
-	, m_terminalAspect(new TerminalAspect(this, C_TERMINALASPECT_ID))
-	, m_localEnvironmentAspect(new LocalEnvironmentAspect(this, LocalEnvironmentAspect::BaseEnvironmentModifier()))
+    : ProjectExplorer::RunConfiguration(target, C_RUNCONFIGURATIONPREFIX)
 {
-	m_terminalAspect->setRunMode(ApplicationLauncher::Gui);
-
-    addExtraAspect(m_argumentAspect);
-    addExtraAspect(m_terminalAspect);
-    addExtraAspect(m_localEnvironmentAspect);
+    auto terminalAspect = new TerminalAspect(this, "Ruby.RunConfiguration.TerminalAspect");
+    terminalAspect->setRunMode(ApplicationLauncher::Gui);
+    addExtraAspect(terminalAspect);
+    addExtraAspect(new ArgumentsAspect(this, "Ruby.RunConfiguration.ArgumentAspect"));
+    addExtraAspect(new ExecutableAspect(this));
+    addExtraAspect(new LocalEnvironmentAspect(this, LocalEnvironmentAspect::BaseEnvironmentModifier()));
+    addExtraAspect(new WorkingDirectoryAspect(this, "Ruby.RunConfiguration.WorkingDirectoryAspect"));
 }
 
 QWidget *RunConfiguration::createConfigurationWidget()
 {
-    return new RunConfigurationWidget(this);
+    return wrapWidget(new RunConfigurationWidget(this));
 }
 
 Runnable RunConfiguration::runnable() const
 {
-	StandardRunnable result;
-    result.runMode = m_terminalAspect->runMode();
-    result.executable = m_script;
-    result.commandLineArguments = m_argumentAspect->arguments();
-    result.workingDirectory = m_workingDirectoryAspect->workingDirectory().toString();
-    result.environment = m_localEnvironmentAspect->environment();
+    StandardRunnable result;
+    result.runMode = extraAspect<TerminalAspect>()->runMode();
+    result.executable = extraAspect<ExecutableAspect>()->executable().toString();
+    result.commandLineArguments = extraAspect<ArgumentsAspect>()->arguments();
+    result.workingDirectory = extraAspect<WorkingDirectoryAspect>()->workingDirectory().toString();
+    result.environment = extraAspect<EnvironmentAspect>()->environment();
     return result;
 }
 
-QString RunConfiguration::extraId() const
+void RunConfiguration::doAdditionalSetup(const RunConfigurationCreationInfo &info)
 {
-    return m_script;
-}
-
-bool RunConfiguration::fromMap(const QVariantMap &map)
-{
-	if (!ProjectExplorer::RunConfiguration::fromMap(map))
-        return false;
-    QString extraId = idFromMap(map).suffixAfter(id());
-    if (!extraId.isEmpty())
-        m_script = extraId;
-
-    setDefaultDisplayName(tr("Run %1").arg(Utils::FileName::fromString(m_script).fileName()));
-    return true;
+    const FileName script = FileName::fromString(info.buildKey);
+    extraAspect<ExecutableAspect>()->setExecutable(script);
+    setDefaultDisplayName(tr("Run %1").arg(script.fileName()));
 }
 
 RunConfigurationWidget::RunConfigurationWidget(RunConfiguration *rc, QWidget *parent)
@@ -80,8 +62,10 @@ RunConfigurationWidget::RunConfigurationWidget(RunConfiguration *rc, QWidget *pa
     auto fl = new QFormLayout(this);
     fl->setMargin(0);
     fl->setFieldGrowthPolicy(QFormLayout::ExpandingFieldsGrow);
-	rc->extraAspect<ArgumentsAspect>()->addToMainConfigurationWidget(this, fl);
-	rc->extraAspect<TerminalAspect>()->addToMainConfigurationWidget(this,fl);
+    rc->extraAspect<ExecutableAspect>()->addToMainConfigurationWidget(this, fl);
+    rc->extraAspect<ArgumentsAspect>()->addToMainConfigurationWidget(this, fl);
+    rc->extraAspect<WorkingDirectoryAspect>()->addToMainConfigurationWidget(this, fl);
+    rc->extraAspect<TerminalAspect>()->addToMainConfigurationWidget(this, fl);
 }
 
 RunConfigurationFactory::RunConfigurationFactory()
